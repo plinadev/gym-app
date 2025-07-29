50aab357ddf55fe6c466ba6939f08dff42b6cf25
plinadev <pavlenkopolina415@gmail.com>
2025-07-29 10:03:57 +0300

    Add UI theme with Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 function Homepage() {
   return (
-    <div>
+    <div className="p-5 flex flex-col gap-5">
       <h1>Homepage</h1>
+      <Button className="w-max">Button</Button>
+      <Input />
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
